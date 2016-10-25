cc39b49e6976cdda978ba84a7d8cb233837dfb64
bigdatajunkies <bigdatajunkies@gmail.com>
2016-10-25 16:16:50 -0400

    Update wordcountMapper.java
    updated comments

--- wordcountMapper.java
+++ wordcountMapper.java
@@ -37,4 +37,17 @@ public class WordCountMapper extends MapReduceBase implements Mapper<LongWritabl
  The input key would a long value assigned in default based on the position of Text in input file. 
  Our output from the mapper is of the format “Word, 1“ hence the data type of our 
  output key value pair is <Text(String),  IntWritable(int)>
+ 
+ The first and second parameter refers to the Data type of the input Key and Value to the mapper. 
+ The third parameter is the output collector which does the job of taking the  output data either from the mapper or reducer, 
+ with the output collector we need to specify the Data Types of the output Key and Value from the mapper. 
+ The fourth parameter, the reporter is used to report the task status internally in Hadoop environment to avoid time outs.
+ 
+ The functionality of the map method is as follows
+1.       Create a IntWritable variable ‘one’ with value as 1
+2.       Convert the input line in Text type to a String
+3.       Use a tokenizer to split the line into words
+4.       Iterate through each word and a form key value pairs as
+a.       Assign each work from the tokenizer(of String type) to a Text ‘word’
+b.      Form key value pairs for each word as <word,one> and push it to the output collector
 **/
