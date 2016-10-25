0ce2669288437142457d77c52f6593199ca52d1f
bigdatajunkies <bigdatajunkies@gmail.com>
2016-10-25 16:48:29 -0400

    Update wordcountReducer.java
    added comments

--- wordcountReducer.java
+++ wordcountReducer.java
@@ -19,3 +19,20 @@ public class WordCountReducer extends MapReduceBase implements Reducer<Text, Int
           output.collect(key, new IntWritable(sum));
       }
 }
+
+
+/**
+Reducer<Text, IntWritable, Text, IntWritable>
+The first two refers to data type of Input Key and Value to the reducer and the last two refers to data type of output key and value. 
+Our mapper emits output as <apple,1> , <grapes,1> , <apple,1> etc.
+
+This is the input for reducer so here the data types of key and value in java would be String and int, 
+the equivalent in Hadoop would be Text and IntWritable. Also we get the output as<word, 
+no of occurrences> so the data type of output Key Value would be <Text, IntWritable>
+
+The input to reduce method from the mapper after the sort and shuffle phase would be the key with the list of 
+associated values with it. For example here we have multiple values for a single key from our mapper like 
+<apple,1> , <apple,1> , <apple,1> , <apple,1> . This key values would be fed into the reducer as < apple, {1,1,1,1} > 
+
+
+**/
